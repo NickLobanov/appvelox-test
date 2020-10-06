1ef2c7ec9da14dde2d99ebb1c75a49fd9a2d27bb
Nick Lobanov <nk.nicolas142@gmail.com>
2020-10-06 21:24:49 +0300

    fix foto

--- src/data/data.js
+++ src/data/data.js
@@ -4,35 +4,35 @@ const data = [
         speciality: 'Хирургия',
         hospital: 'СПБ ГБУЗ "Городская поликлиника №25", пр. Солидарности, д. 1, к. 1, лит. А',
         time: 'Понедельник 15.06.20 | 15:30',
-        url: "malushko.png"
+        url: "https://lh3.googleusercontent.com/proxy/ImqF_4qa_WqMyHI98PQk-pnrSbnocOckZbYZht4OWdaWmLdTYa3EZLmHsskJOAnEuL5ZTrsrTd5W_E5ok32NJWAzXLX5"
     },
     {
         name: 'Харьков В. С.',
         speciality: 'Терапевтическое отделение',
         hospital: 'СПБ ГБУЗ "Городская поликлиника №25", пр. Солидарности, д. 1, к. 1, лит. А',
         time: 'Понедельник 15.06.20 | 18:30',
-        url: "charkov.png"
+        url: "https://zentromed.com/tmp/img/03uro16_1.jpg"
     },
     {
         name: 'Малушко Т. Н.',
         speciality: 'Хирургия',
         hospital: 'СПБ ГБУЗ "Городская поликлиника №25", пр. Солидарности, д. 1, к. 1, лит. А',
         time: 'Понедельник 15.06.20 | 15:30',
-        url: "malushko.png"
+        url: "https://lh3.googleusercontent.com/proxy/ImqF_4qa_WqMyHI98PQk-pnrSbnocOckZbYZht4OWdaWmLdTYa3EZLmHsskJOAnEuL5ZTrsrTd5W_E5ok32NJWAzXLX5"
     },
     {
         name: 'Харьков В. С.',
         speciality: 'Терапевтическое отделение',
         hospital: 'СПБ ГБУЗ "Городская поликлиника №25", пр. Солидарности, д. 1, к. 1, лит. А',
         time: 'Понедельник 15.06.20 | 18:30',
-        url: "charkov.png"
+        url: "https://zentromed.com/tmp/img/03uro16_1.jpg"
     },
     {
         name: 'Малушко Т. Н.',
         speciality: 'Хирургия',
         hospital: 'СПБ ГБУЗ "Городская поликлиника №25", пр. Солидарности, д. 1, к. 1, лит. А',
         time: 'Понедельник 15.06.20 | 15:30',
-        url: "malushko.png"
+        url: "https://lh3.googleusercontent.com/proxy/ImqF_4qa_WqMyHI98PQk-pnrSbnocOckZbYZht4OWdaWmLdTYa3EZLmHsskJOAnEuL5ZTrsrTd5W_E5ok32NJWAzXLX5"
     }
 ]
 
